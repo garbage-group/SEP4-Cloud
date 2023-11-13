804f425b30ede9666ebbd55db1616b63b1cffcc3
Martin Vlacil <martinvlacil.mv@gmail.com>
2023-11-13 15:45:53 +0100

    adding passing test

--- cloud/src/test/java/garbagegroup/cloud/CloudApplicationTests.java
+++ cloud/src/test/java/garbagegroup/cloud/CloudApplicationTests.java
@@ -3,11 +3,14 @@ package garbagegroup.cloud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CloudApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(1, 1);
     }
 
 }
