47f4f1e0fc23b97f04945b4074c6e9ed2f2790f2
Martin Vlacil <martinvlacil.mv@gmail.com>
2023-11-27 09:41:42 +0100

    Pushing before mergin more features into this branch

--- src/main/java/garbagegroup/cloud/tcpserver/ITCPServer.java
+++ src/main/java/garbagegroup/cloud/tcpserver/ITCPServer.java
@@ -2,7 +2,6 @@ package garbagegroup.cloud.tcpserver;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public interface ITCPServer {
     public String getHumidityById(int deviceId);
     void startServer();
--- src/test/java/garbagegroup/cloud/services/BinServiceTest.java
+++ src/test/java/garbagegroup/cloud/services/BinServiceTest.java
@@ -1,19 +1,68 @@
 package garbagegroup.cloud.services;
 
 import garbagegroup.cloud.model.Bin;
+import garbagegroup.cloud.model.Humidity;
 import garbagegroup.cloud.repository.IBinRepository;
 import garbagegroup.cloud.service.BinService;
 import garbagegroup.cloud.service.IBinService;
+import garbagegroup.cloud.tcpserver.ITCPServer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
 
-public class BinServiceTest {
+class BinServiceTest {
+
+    @Mock
+    private ITCPServer tcpServer;
+
+    @Mock
+    private IBinRepository binRepository;
+
+    @InjectMocks
+    private BinService binService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
-    public void getHumidityById1Returns25() {
-
+    void getCurrentHumidityByBinId1ReturnsHumidity() {
+        // Mocking data
+//        Bin bin = new Bin();
+//        bin.setId(1L);
+//        bin.setDeviceId(1);
+//
+//        Humidity humidity1 = new Humidity();
+//        humidity1.setDateTime(LocalDateTime.now().minusMinutes(30));
+//        humidity1.setValue(50.0);
+//
+//        Humidity humidity2 = new Humidity();
+//        humidity2.setDateTime(LocalDateTime.now().minusMinutes(90));
+//        humidity2.setValue(60.0);
+//
+//        bin.setHumidity(Arrays.asList(humidity1, humidity2));
+//
+//        // Mock repository behavior
+//        when(binRepository.findById(1L)).thenReturn(Optional.of(bin));
+//        when(tcpServer.getHumidityById(1)).thenReturn("Humidity data from IoT");
+//
+//        // Perform the test
+//        Optional<Humidity> result = binService.getCurrentHumidityByBinId(1L);
+//
+//        // Verify the interactions and assertions
+//        verify(binRepository, times(1)).findById(1L);
+//        verify(tcpServer, times(1)).getHumidityById(1);
+//        verify(binRepository, times(1)).save(any(Bin.class)); // Assuming save method is called when handling IoT data
+//
+//        // Assert the result
+//        assertEquals(50.0, result.orElseThrow().getValue());
     }
 }
