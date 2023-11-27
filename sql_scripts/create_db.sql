6edafc5c8ecd2b79264de34df4f9dac5ad7d4be5
Tomas Chmelik <1086311@ucn.dk>
2023-11-27 09:09:22 +0100

    CICD finalisation, sql scripts update

--- sql_scripts/create_db.sql
+++ sql_scripts/create_db.sql
@@ -1,14 +1,14 @@
 CREATE TABLE public.bin (
-                            id int8 NOT NULL GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE),
-                            "location" varchar NULL,
-                            capacity numeric NULL,
-                            emptied_last timestamp NULL,
-                            pick_up_time timestamp NULL,
-                            fill_threshold numeric NULL,
-                            device_id int8 NULL,
-                            CONSTRAINT bin_pk PRIMARY KEY (id)
+                                id int8 NOT NULL GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE),
+                                capacity float8 NULL,
+                                emptied_last timestamp NULL,
+                                pick_up_time timestamp NULL,
+                                fill_threshold float8 NULL,
+                                device_id int4 NULL,
+                                latitude float8 NULL,
+                                longitude float8 NULL,
+                                CONSTRAINT bin_pk PRIMARY KEY (id)
 );
-
 CREATE TABLE public.humidity (
                                  value numeric NULL,
                                  date_time timestamp NOT NULL,
@@ -31,4 +31,13 @@ CREATE TABLE public.temperature (
                                     bin_id int8 NOT NULL,
                                     CONSTRAINT temperature_pk PRIMARY KEY (bin_id, date_time),
                                     CONSTRAINT temperature_fk FOREIGN KEY (bin_id) REFERENCES public.bin(id) ON UPDATE CASCADE
+);
+
+CREATE TABLE public.users (
+                                    username varchar(255) NOT NULL,
+                                    fullname varchar(255) NULL,
+                                    "password" varchar(255) NULL,
+                                    "role" varchar(255) NULL,
+                                    region varchar(255) NULL,
+                                    CONSTRAINT users_pkey PRIMARY KEY (username)
 );
