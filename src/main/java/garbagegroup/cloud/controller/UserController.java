accc67fb9f8f66247c22836a60df810100f90957
iamsu <iamsuhani5@gmail.com>
2023-12-04 13:12:21 +0100

    feature - get All users

--- src/main/java/garbagegroup/cloud/controller/UserController.java
+++ src/main/java/garbagegroup/cloud/controller/UserController.java
@@ -1,5 +1,6 @@
 package garbagegroup.cloud.controller;
 
+import garbagegroup.cloud.DTOs.DTOConverter;
 import garbagegroup.cloud.DTOs.UserDto;
 import garbagegroup.cloud.jwt.auth.AuthenticationResponse;
 
@@ -7,10 +8,13 @@ import garbagegroup.cloud.model.User;
 import garbagegroup.cloud.service.serviceImplementation.UserService;
 import garbagegroup.cloud.service.serviceInterface.IUserService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,21 +24,25 @@ public class UserController {
 
     private final IUserService userService;
     private final UserService service;
+    private DTOConverter dtoConverter;
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
 
     @Autowired
     public UserController(IUserService userService, UserService service) {
         this.userService = userService;
         this.service = service;
+        this.dtoConverter = new DTOConverter();
     }
 
 
     @GetMapping("/users/{username}")
+    @PreAuthorize("hasRole('garbage collector')")
     public ResponseEntity<UserDto> fetchUserByUsername(@PathVariable("username") String username) {
         try {
             User user = userService.fetchUserByUsername(username);
-            UserDto userDto = service.convertToUserDto(user); // Call the convertToUserDto method from UserService
+            UserDto userDto = dtoConverter.convertToUserDto(user); // Call the convertToUserDto method from UserService
             return new ResponseEntity<>(userDto, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -48,6 +56,16 @@ public class UserController {
             return ResponseEntity.ok(service.authenticate(request));
         } catch (Exception e) {
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> fetchAllUsers() {
+        try {
+            return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
+        } catch (RuntimeException e) {
+            logger.error("Error retrieving all users", e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
--- src/main/java/garbagegroup/cloud/service/serviceInterface/IUserService.java
+++ src/main/java/garbagegroup/cloud/service/serviceInterface/IUserService.java
@@ -1,10 +1,15 @@
 package garbagegroup.cloud.service.serviceInterface;
 
+import garbagegroup.cloud.DTOs.UserDto;
 import garbagegroup.cloud.model.User;
+
+import java.util.List;
 
 
 public interface IUserService {
 
     User fetchUserByUsername(String username);
+
+    List<UserDto> fetchAllUsers();
 
 }
